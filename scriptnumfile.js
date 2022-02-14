a78dd56afc5ef51c547c4ccf40f1aca4e30888db
Anton Soldatov <antonsoldatov1983@gmail.com>
2022-02-14 02:02:13 +0200

    workWithFunction

--- scriptnumfile.js
+++ scriptnumfile.js
@@ -132,4 +132,64 @@ const calcl = (a , b) => a+b;
 const calc = (a , b) => {
 return a+b;
 }; */
-window.crypto.getRandomValues(100);
+/* const test="12px";
+console.log(parseInt(test)); */
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?',"");
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?',"");
+  }
+}
+
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actor:{},
+    genres:[],
+    privat: false
+};
+
+function remembermyfilms(){
+    for (let i = 1;i < 3;i ++) {
+        let a = prompt('Один из последних просмотренных фильмов?',""),
+            b = prompt('На сколько оцените его?',"");
+    
+            if (a != null && b != null && a != "" && b != "" && a.length < 50){
+                personalMovieDB.movies[a] = b;
+                console.log("done!");
+            } else {
+                console.log("error");
+                i--;
+            } 
+    }
+}
+remembermyfilms();
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count <= 30 && personalMovieDB.count >= 10){
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("ошибка");
+    }
+}
+detectPersonalLevel();
